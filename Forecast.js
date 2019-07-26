20d191d35244a31be527d7a3a811f81542258ec8
imanipaul <imanipaul@wustl.edu>
2019-07-26 12:54:47 -0400

    Update styles in Forecast

--- Forecast.js
+++ Forecast.js
@@ -14,11 +14,26 @@ class Forecast extends Component {
                 <Text style={styles.bigText}>
                     {this.props.temp}ºF
             </Text>
-
-
             </View>
         )
     }
-
-
 }
+
+const styles = StyleSheet.create({
+    container: { height: 130 },
+    bigText: {
+        flex: 2,
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+        color: '#ffffff',
+    },
+    mainText: {
+        flex: 1,
+        fontSize: 16,
+        textAlign: 'center',
+        color: '#ffffff'
+    },
+})
+
+export default Forecast;
